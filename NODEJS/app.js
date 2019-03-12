57aa8837c691b807ed696a6faa3cf9404c7b5a05
bocin45 <32359666+bocin45@users.noreply.github.com>
2019-03-12 20:03:02 +0800

    add html and css

--- NODEJS/app.js
+++ NODEJS/app.js
@@ -7,15 +7,16 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
 
-app.use((request, response, next)=>{
+app.use((request, response, next) => {
     // response.send('<h1>Page Not found dude</h1>').status(404);
-    response.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    response.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // const server = http.createServer(app);
--- NODEJS/routes/admin.js
+++ NODEJS/routes/admin.js
@@ -5,7 +5,7 @@ const rootDir = require('../path/path.js');
 const router  = express.Router();
 
 //admin/add-product => GET
-router.get('/add_product',(request, response, next)=>{
+router.get('/add-product',(request, response, next)=>{
     // response.send('<form action=/admin/add-product method=POST><input type="text" name="message"><button type="Submit">Tambah Produk</button></form>');
     response.sendfile(path.join(rootDir,'views','add-product.html'))
 });
@@ -13,6 +13,7 @@ router.get('/add_product',(request, response, next)=>{
 //admin/product => POST
 router.post('/add-product',(request, response, next)=>{
     console.log(request.body);
+    console.log("tesss");
     response.redirect('/shop');
 });
 
